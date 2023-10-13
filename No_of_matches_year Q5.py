d27283cea83728e9ad2b2dbb73e7a2d22957b139
lokesh-bottu <lokeshbottu1122@gmail.com>
2023-10-13 08:40:07 +0530

    Que 5 solved

---
+++ No_of_matches_year Q5.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import csv
+from collections import defaultdict
+file = open("matches.csv")
+delivery = csv.DictReader(file)
+
+matches_played = {}
+for match in delivery:
+    year = match["season"]
+    matches_played[year] = matches_played.get(year,0) + 1
+
+    
+for i in matches_played:
+    print(i," : ",matches_played[i])
+
+years = list(matches_played.keys())
+no_of_matches = list(matches_played.values())
+
+plt.bar(years, no_of_matches)
+plt.xlabel('Year')
+plt.ylabel('No of matched Played ')
+plt.title('No of matches played per year for all years')
+for i, no_of_matches in enumerate(no_of_matches):
+    plt.text(i, no_of_matches, str(no_of_matches), ha='center', va='bottom')
+plt.show()
